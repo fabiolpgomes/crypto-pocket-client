96e9db14fe82afedbd515a9a1d1eb85ae9ae704e
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-10-02 20:41:01 +0200

    Created api

---
+++ src/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+//configurando nossas URLs de API
+const apiURLs = {
+  development: "http://localhost:4000",
+  production: "URL Deployed Heroku",
+};
+
+const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
+
+//adicionar SE HOUVER, o token no header
+api.interceptors.request.use((config) => {
+  const loggedInUserJSON = localStorage.getItem("loggedInUser"); //pega o user, mas ele tá em JSON
+  const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""'); // pega o json e transforma em OBJETO.
+
+  /*  if (!parseLoggedInUser) {
+    return config;
+  } */
+
+  if (parseLoggedInUser.token) {
+    config.headers = { Authorization: `Bearer ${parseLoggedInUser.token}` };
+  }
+
+  return config;
+});
+
+export { api };
