4396024b2663d36cc481df42bae23338d707a4f3
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-10-06 23:20:45 +0200

    Change URL production https://cryptopocketbf.herokuapp.com/

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //configurando nossas URLs de API
 const apiURLs = {
-  development: "http://localhost:4000/",
-  production: "URL Deployed Heroku",
+  development: "http://localhost:4000",
+  production: "https://cryptopocketbf.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
