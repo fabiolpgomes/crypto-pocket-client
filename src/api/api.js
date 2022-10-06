4a3b7f94f4b5d59783f2fdd11d96ad1a45e7ba64
Bruno Apostolo <bruno.apos13@gmail.com>
2022-10-06 18:29:49 -0300

    deploy now

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //configurando nossas URLs de API
 const apiURLs = {
   development: "http://localhost:4000/",
-  production: "https://cryptopocket.netlify.app",
+  production: "https://cryptopocketbf.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
