40cfff2c699c4feb97165cd4c5e9f617c3d76933
Bruno Apostolo <bruno.apos13@gmail.com>
2022-10-06 22:55:46 -0300

    done

--- src/api/api.js
+++ src/api/api.js
@@ -5,13 +5,10 @@ const apiURLs = {
   development: "http://localhost:4000",
 
   production: "https://cryptopocketbf.herokuapp.com",
-
-
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
 
-//adicionar SE HOUVER, o token no header
 api.interceptors.request.use((config) => {
   const loggedInUserJSON = localStorage.getItem("loggedInUser"); //pega o user, mas ele tá em JSON
   const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""'); // pega o json e transforma em OBJETO.
