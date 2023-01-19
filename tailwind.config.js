965a61a3498871e128dd3a0f36d64ac3b886a23f
Fabio Gomes <fabiolpgomes@hotmail.com>
2023-01-19 05:39:58 +0100

    add image homepageview

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms"), require("@headlessui/react")],
+export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+export const theme = {
+  extend: {},
 };
+export const plugins = [require("@tailwindcss/forms"), require("@headlessui/react")];
